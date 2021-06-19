aa0097e689ffda8c2c80d4e8dfb7a6b319b6da83
Ebonie Butler <ebonie@yikesinc.com>
2021-06-18 21:45:43 -0400

    log reporting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import { config } from "dotenv";
-import tmi from "tmi.js";
+import fs from 'fs';
+import { config } from 'dotenv';
+import tmi from 'tmi.js';
 import { autoShoutOut } from './components/autoshoutout.js';
 import './components/twitchAPI.js';
 import './components/streamElementsAPI.js';
@@ -19,8 +20,12 @@ const opts = {
 // Create a client with our options.
 const client = new tmi.client(opts);
 
+// Connect to Twitch.
+client.connect();
+
 // Called everytime the bot connects to Twitch chat.
 const onConnected = (addr, port) => {
+  console.log(`connected to Twitch chat.`);
   client.say(process.env.CHANNEL_NAME, `Hi there.`);
 }
 
@@ -28,3 +33,14 @@ client.on(`connected`, onConnected);
 
 // Called everytime a message is sent in Twitch chat.
 client.on(`message`, autoShoutOut.bind(client));
+
+// Logging
+client.on(`join`, (channel, username, self) => {
+  const today = new Date();
+  ;
+  fs.appendFileSync(`log.txt`, `[${today.toLocaleString('en-US', { timeZone: 'America/New_York' })}] ${username} has joined channel. \n`);
+});
+client.on(`part`, (channel, username, self) => {
+  const today = new Date();
+  fs.appendFileSync(`log.txt`, `[${today.toLocaleString('en-US', { timeZone: 'America/New_York' })}] ${username} has left channel. \n`);
+});
