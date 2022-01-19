1c506ea13f343a3d34703518a30b9075a20ac5e7
Ebonie Butler <ebonie@yikesinc.com>
2022-01-18 20:02:28 -0500

    code comment

--- vendor/tmi.js
+++ vendor/tmi.js
@@ -7,12 +7,14 @@ config();
 // Define configuration options.
 const opts = {
   options: {},
-identity: {
-  username: process.env.BOT_USERNAME,
-  password: process.env.OAUTH_TOKEN
-},
-channels: [process.env.CHANNEL_NAME]
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN
+  },
+  channels: [process.env.CHANNEL_NAME]
 };
 
 // Create a client with our options.
 export const tmi = new Client.client(opts);
+
+// Convert to class. Create contructor and method to send message.
