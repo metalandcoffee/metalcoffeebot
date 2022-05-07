cd248f69395d82a21f1277d5a91a945012ef35ff
Ebonie Butler <ebonie@yikesinc.com>
2022-05-07 10:48:10 -0400

    eslint

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // External dependencies.
 import path from 'path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 import fs from 'fs';
-import { config } from 'dotenv';
+import {config} from 'dotenv';
 import express from 'express';
-import { tmi } from './vendor/tmi.js';
-import { logColorMsg } from './helpers.js';
+import {tmi} from './vendor/tmi.js';
+import {logColorMsg} from './helpers.js';
 import connectTAU from './vendor/tau.js';
 
 // Internal dependencies.
-import { home, addCommand } from './express.js'
-//import './components/autoshoutout.js';
+import {home, addCommand} from './express.js';
+import './components/autoshoutout.js';
 import './components/commands.js';
 import './components/timers.js';
 
@@ -22,7 +22,7 @@ const app = express();
 const port = 3001;
 app.listen(port, () => {
   logColorMsg(`Example app listening at http://localhost:${port}`);
-})
+});
 // Express: Serve images, CSS files, and JavaScript files in a directory named public.
 app.use(express.static(__dirname + '/public'));
 app.get('/', home);
@@ -35,7 +35,7 @@ config();
 tmi.connect();
 
 // Temp Function
-tmi.on(`message`, function (channel, tags, message, self) {
+tmi.on(`message`, function(channel, tags, message, self) {
   if (!self) {
     logColorMsg(`${tags.username}: ${message}`);
   }
@@ -44,9 +44,9 @@ tmi.on(`message`, function (channel, tags, message, self) {
 // On successful connection...
 const onConnected = (addr, port) => {
   logColorMsg(`Connected to Twitch chat.`);
-  //tmi.say(process.env.CHANNEL_NAME, `Hi there.`);
+  tmi.say(process.env.CHANNEL_NAME, `Hi there.`);
   connectTAU();
-}
+};
 tmi.on(`connected`, onConnected);
 
 // Logging.
@@ -56,13 +56,12 @@ let viewers = [];
 try {
   viewers = fs.readFileSync(`logs/log-${todayStr}.txt`, `utf8`);
   viewers = viewers.split('\n');
-  //console.log(viewers);
+  // console.log(viewers);
 } catch (err) {
   console.error(err);
 }
 
 tmi.on(`join`, (channel, username, self) => {
-
   // If not in viewers array, add and write to file.
   if (!viewers.includes(username)) {
     viewers.push(username);
