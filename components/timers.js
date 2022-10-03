d83e16ace7dfa97f115584a27e94ca1e7d33c473
Ebonie Butler <ebonie@yikesinc.com>
2022-10-03 19:53:14 -0400

    update timers and commands

--- components/timers.js
+++ components/timers.js
@@ -72,7 +72,7 @@ function maybeUpdateExpDate(sendTimerMsg = false) {
 
   // Turn object into string before updating file.
   const stringifiedTimers = JSON.stringify(parsedTimers);
-  fs.writeFile('./components/tmp/timers.json', stringifiedTimers, (err) => {
+  fs.writeFile('./components/db/timers.json', stringifiedTimers, (err) => {
     // console.log(parsedTimers);
     // console.log('writing to timers.json');
     if (err) return console.log(err);
